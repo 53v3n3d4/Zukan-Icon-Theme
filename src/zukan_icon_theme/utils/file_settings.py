9057f652672c112c93d91d1be5d55263af26c4f2
Seven Eda <53v3n3d4@gmail.com>
2024-06-17 23:38:51 -0300

    Add file_settings

---
+++ src/zukan_icon_theme/utils/file_settings.py
@@ -0,0 +1,13 @@
+# Files
+USER_SETTINGS = 'Preferences.sublime-settings'
+ZUKAN_SETTINGS = 'Zukan Icon Theme.sublime-settings'
+ZUKAN_VERSION = 'zukan-version.sublime-settings'
+
+# Options
+ZUKAN_SETTINGS_OPTIONS = [
+    'log_level',
+    'version',
+    'zukan_restart_message',
+    'rebuild_on_upgrade',
+    'auto_install_theme',
+]
