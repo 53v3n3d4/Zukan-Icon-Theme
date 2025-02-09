b08be44c37f84cdb6270eaa0d3a6b796191d1791
Seven Eda <53v3n3d4@gmail.com>
2025-02-09 02:12:11 -0300

    fix: correct tests path handling using os.path.join and os.path.normpath for Windows CI

--- tests/zukan_icon_theme/test_zukan_paths.py
+++ tests/zukan_icon_theme/test_zukan_paths.py
@@ -1,11 +1,9 @@
 import importlib
-
-# import os
+import os
 import sublime
 
 from os.path import relpath
 from unittest import TestCase
-from unittest.mock import Mock, patch
 
 zukan_paths = importlib.import_module(
     'Zukan Icon Theme.src.zukan_icon_theme.utils.zukan_paths'
@@ -14,72 +12,78 @@ file_extensions = importlib.import_module(
     'Zukan Icon Theme.src.zukan_icon_theme.utils.file_extensions'
 )
 
-
-DATA_PATH = zukan_paths.filepath('../../data')
+DATA_PATH = os.path.normpath(zukan_paths.filepath('../../data'))
 PACKAGE_NAME = 'Zukan Icon Theme'
-ST_PACKAGES_PATH = sublime.packages_path() + '/'
-ST_INSTALLED_PACKAGES_PATH = sublime.installed_packages_path() + '/'
+ST_PACKAGES_PATH = sublime.packages_path()
+ST_INSTALLED_PACKAGES_PATH = sublime.installed_packages_path()
 
 params_list = [
-    (zukan_paths.PACKAGES_PATH + '/', ST_PACKAGES_PATH),
-    (zukan_paths.INSTALLED_PACKAGES_PATH + '/', ST_INSTALLED_PACKAGES_PATH),
+    (os.path.join(zukan_paths.PACKAGES_PATH), ST_PACKAGES_PATH),
+    (os.path.join(zukan_paths.INSTALLED_PACKAGES_PATH), ST_INSTALLED_PACKAGES_PATH),
     (
         zukan_paths.ZUKAN_PKG_ICONS_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME + '/icons',
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME, 'icons'),
     ),
     (
         zukan_paths.ZUKAN_PKG_ICONS_PREFERENCES_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME + '/icons_preferences',
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME, 'icons_preferences'),
     ),
     (
         zukan_paths.ZUKAN_PKG_ICONS_SYNTAXES_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME + '/icons_syntaxes',
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME, 'icons_syntaxes'),
     ),
     (
         zukan_paths.ZUKAN_PKG_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME,
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME),
     ),
     (
         zukan_paths.ZUKAN_PKG_SRC_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME + '/src',
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME, 'src'),
     ),
     (
         zukan_paths.ZUKAN_PKG_SUBLIME_PATH,
-        ST_PACKAGES_PATH + PACKAGE_NAME + '/sublime',
+        os.path.join(ST_PACKAGES_PATH, PACKAGE_NAME, 'sublime'),
     ),
     (
         zukan_paths.ZUKAN_INSTALLED_PKG_PATH,
-        ST_INSTALLED_PACKAGES_PATH
-        + PACKAGE_NAME
-        + file_extensions.SUBLIME_PACKAGE_EXTENSION,
+        os.path.join(
+            ST_INSTALLED_PACKAGES_PATH,
+            PACKAGE_NAME + file_extensions.SUBLIME_PACKAGE_EXTENSION,
+        ),
     ),
     (
         zukan_paths.ZUKAN_ICONS_DATA_FILE,
-        ST_PACKAGES_PATH
-        + PACKAGE_NAME
-        + '/icons_data/zukan_icons_data'
-        + file_extensions.PICKLE_EXTENSION,
+        os.path.join(
+            ST_PACKAGES_PATH,
+            PACKAGE_NAME,
+            'icons_data',
+            'zukan_icons_data' + file_extensions.PICKLE_EXTENSION,
+        ),
     ),
     (
         zukan_paths.ZUKAN_VERSION_FILE,
-        ST_PACKAGES_PATH
-        + PACKAGE_NAME
-        + '/sublime/zukan-version'
-        + file_extensions.SUBLIME_SETTINGS_EXTENSION,
+        os.path.join(
+            ST_PACKAGES_PATH,
+            PACKAGE_NAME,
+            'sublime',
+            'zukan-version' + file_extensions.SUBLIME_SETTINGS_EXTENSION,
+        ),
     ),
     (
         zukan_paths.ZUKAN_CURRENT_SETTINGS_FILE,
-        ST_PACKAGES_PATH
-        + PACKAGE_NAME
-        + '/sublime/zukan_current_settings'
-        + file_extensions.PICKLE_EXTENSION,
+        os.path.join(
+            ST_PACKAGES_PATH,
+            PACKAGE_NAME,
+            'sublime',
+            'zukan_current_settings' + file_extensions.PICKLE_EXTENSION,
+        ),
     ),
 ]
 
 
 class TestZukanPaths(TestCase):
     def test_param_is_string(self):
-        TESTS_FOLDER = zukan_paths.filepath('../tests')
+        TESTS_FOLDER = os.path.normpath(zukan_paths.filepath('../tests'))
         self.assertIsInstance(TESTS_FOLDER, str)
 
     def test_param_fails_int(self):
@@ -95,9 +99,9 @@ class TestZukanPaths(TestCase):
     # test possible in diferent machines.
     # https://docs.python.org/3/library/os.path.html#os.path.relpath
     def test_relative_path(self):
-        TESTS_FOLDER = zukan_paths.filepath('../tests')
+        TESTS_FOLDER = os.path.normpath(zukan_paths.filepath('../tests'))
         SHORT_URL = relpath(TESTS_FOLDER, start=DATA_PATH)
-        TESTS_RELATIVE_PATH = '../zukan_icon_theme/tests'
+        TESTS_RELATIVE_PATH = os.path.normpath('../zukan_icon_theme/tests')
         self.assertEqual(SHORT_URL, TESTS_RELATIVE_PATH)
 
     def test_zukan_path(self):
